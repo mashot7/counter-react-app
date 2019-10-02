2ad9dac1548fa8628700040abcc312270948aecb
mashot7 <mashot2@gmail.com>
2019-10-02 22:47:50 +0400

    Update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,22 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class counter extends Component {
   state = {
-    count: 0
+    count: 0,
     // imageUrl: "https://picsum.photos/200"
-  };
+    tag: ['lemon', 'banana', 'pineapple']
+  }
 
-  render() {
+  render () {
     return (
       <React.Fragment>
-        {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tag.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>
       </React.Fragment>
-    );
+    )
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
 }
 
-export default counter;
+export default counter
